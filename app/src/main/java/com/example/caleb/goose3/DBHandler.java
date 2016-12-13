5526e5814bf76902e91b4d182e408c6c3093986f
AneurysmAssimilation <zachary.sylvester@maine.edu>
2016-12-13 11:54:30 -0500

    Up

--- app/src/main/java/com/example/caleb/goose3/DBHandler.java
+++ app/src/main/java/com/example/caleb/goose3/DBHandler.java
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHandler extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 5;
+    private static final int DATABASE_VERSION = 6;
     private static final String DATABASE_NAME = "geese.db";
 
     public DBHandler(Context context ) {
